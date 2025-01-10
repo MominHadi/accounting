98df154a84c525215bf0c1814f103fcaef0ebe1c
abdulwahid2003 <mominwahid8080@gmail.com>
2025-01-11 00:15:51 +0530

    avx

--- middleware/multer.js
+++ middleware/multer.js
@@ -5,7 +5,7 @@ const path = require('path');
 const fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
         let folder = 'uploads';
-
+        console.log(req,file, 'Data tesing');
         // Check file type to determine folder
         if (['image/png', 'image/jpg', 'image/jpeg'].includes(file.mimetype)) {
             folder = 'uploads/images';
